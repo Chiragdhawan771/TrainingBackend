89042d34340c4ac928dfca7fdd15c6e29205c530
Chiragdhawan771 <116626589+Chiragdhawan771@users.noreply.github.com>
2024-11-07 01:03:16 +0530

    jwt and database connectivity

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 
 import dotenv from "dotenv"
 import connectDB from "./DataBase/index.js"
-// import mongoose from "mongoose"
-// import { DB_NAME } from "./constants.js"
-// import express from "express"
+import { app } from "./app.js"
+
+
 dotenv.config({
     path: "./env"
 })
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is Running at ${process.env.PORT}`)
+    })
+}).catch((err) => { console.log(err, "Connection Failed") })
+
 // const app = express()
 // iffies
 // (async () => {
